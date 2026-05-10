57e0f6cfeb1175be79cc226f5f4240b6fb8af0ea
ibrohimshomurotov4-code <ibrohimshomurotov4@gmail.com>
2026-05-10 14:45:15 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,1 @@
-import React from 'react'; export default function App(){return React.createElement('div',null,'Love Hub ishlayapti!')}
+import { useState, useEffect, useRef, useCallback } from "react";
